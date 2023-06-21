0a1117f5f6f154765c9946afa4d9f916bd7a77ff
exceptionError-jin <kj98224849@gmail.com>
2023-06-21 23:10:18 +0900

    메인 화면 정보 출력

--- management/src/App.js
+++ management/src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = {
+    name: '김진',
+    birthday: '980216',
+    gender: '남',
+    job: '무직(로또 당첨 예정)'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>
-          React-project
-        </h2>
-      </header>
-    </div>
+    <Customer
+    name={customer.name}
+    birthday={customer.birthday}
+    gender={customer.gender}
+    job={customer.job}
+    />
   );
 }
 
