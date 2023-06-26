bcedba2fbca78d439f18e8f241090bc719cd8c4b
exceptionError-jin <kj98224849@gmail.com>
2023-06-26 22:25:32 +0900

    프로젝트 DB연동

--- management/server.js
+++ management/server.js
@@ -1,40 +1,32 @@
+const fs = require('fs'); //파일에 접근할수 있게해줌(DB)
 const express = require('express'); // Express 프레임워크를 가져옴
 const cors = require('cors'); // CORS 모듈을 가져옴
 
 const app = express(); // Express 애플리케이션 생성
 const port = process.env.PORT || 5000; // 포트 설정
 
+const data = fs.readFileSync('./database.json'); //DB값 가져오기
+const conf = JSON.parse(data); //json으로 변환
+const mysql = require('mysql'); //mysql라이브러리
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+}); //DB값 연동
+connection.connect; //연결
+
 app.use(cors()); // CORS 미들웨어를 Express 애플리케이션에 적용
 
 app.get('/api/customers', (req, res) => {
-  const customers = [ // customers 배열 생성
-    {
-      id: 1,
-      name: "김진",
-      image: "https://picsum.photos/id/1/64/64",
-      birthday: "980216",
-      gender: "남",
-      job: "무직(로또 당첨 예정)"
-    },
-    {
-      id: 2,
-      name: "진",
-      image: "https://picsum.photos/id/2/64/64",
-      birthday: "980211",
-      gender: "여",
-      job: "맥도날드 대표"
-    },
-    {
-      id: 3,
-      name: "김",
-      image: "https://picsum.photos/id/3/64/64",
-      birthday: "980212",
-      gender: "남",
-      job: "아마존 대표"
+  connection.query(
+    "SELECT * FROM CUSTOMER", 
+    (err, rows, fields) => {
+      res.send(rows);
     }
-  ];
-
-  res.json(customers); // customers 배열을 JSON 형식으로 응답
+  );
 });
 
 app.listen(port, () => {
